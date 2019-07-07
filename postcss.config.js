8d0a3ebd4f0921988b2d329a9bb9f7f891a13f31
iwanttobealight <iwanttobealight@yandex.ru>
2019-07-07 18:52:57 +0300

    update postcss.config

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-normalize'),
     require('autoprefixer'),
+    require('postcss-nesting'),
     require('postcss-mixins'),
-    require('postcss-nesting')
   ]
 }
