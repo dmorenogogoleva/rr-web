8d5692673a2acb42aace79a794279a3ae6651b68
iwanttobealight <iwanttobealight@yandex.ru>
2019-07-14 14:07:53 +0300

    fix hot-reload

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   plugins: [
     require('autoprefixer'),
-    require('postcss-parcel-import'),
     require('postcss-normalize'),
-    require('postcss-mixins'),
     require('postcss-nesting'),
     require('postcss-preset-env'),
     require('postcss-custom-media'),
