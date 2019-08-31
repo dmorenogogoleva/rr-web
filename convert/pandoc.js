ea7dc84df7a7e8bd271f45fcde5fcdac18ea10d3
iwanttobealight <iwanttobealight@yandex.ru>
2019-08-31 06:46:36 +0300

    move converted files to /

--- convert/pandoc.js
+++ convert/pandoc.js
@@ -2,8 +2,8 @@ var nodePandoc = require('node-pandoc');
 
 var source = './readme.md';
 
-var toEpub = ['-o', 'src/text/rachreiten.epub', 'readme.md', '--metadata-file=meta.yml',  '--toc', '--toc-depth=2', '--epub-cover-image=./cover/1.jpg', '--css=src/styles/text/pdf-styles.css']
-var toFb2 = ['-f', 'markdown', '-t', 'fb2', '-o', 'src/text/rachreiten.fb2'];
+var toEpub = ['-o', 'text/rachreiten.epub', 'readme.md', '--metadata-file=meta.yml',  '--toc', '--toc-depth=2', '--epub-cover-image=./cover/1.jpg', '--css=src/styles/text/pdf-styles.css']
+var toFb2 = ['-f', 'markdown', '-t', 'fb2', '-o', 'text/rachreiten.fb2'];
 
 var errorHandler = function (err, result) {
 
