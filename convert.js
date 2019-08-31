871e07853ff03a1347f711fa45d85f488ae44c01
iwanttobealight <iwanttobealight@yandex.ru>
2019-08-31 08:18:53 +0300

    add epub-gen

--- convert.js
+++ convert.js
@@ -3,3 +3,37 @@ const convertToPdf = require('./convert/convertToPdf');
 
 pandoc.execute()
 convertToPdf.execute()
+
+const fs = require('fs');
+const Epub = require("epub-gen");
+
+
+
+fs.readFile('./rachreiten.html', 'utf-8', (err, data) => {
+  if (err) { throw err; }
+
+  const content = data.split('<h2 id=').map((el) => {
+    let title
+
+    const clone = el
+    clone.replace(/(?!>)([^><]+)(?=<\/h2>)/, (...res) => {
+      title = res[0]
+    })
+
+    return {
+      title,
+      data: el
+    }
+  })
+
+  const option = {
+    title: "Рахрейтен",
+    lang: 'ru',
+    author: "Дарья Морено-Гоголева",
+    cover: "./cover/2.jpg",
+    content
+  };
+
+  new Epub(option, "path.epub");
+})
+
--- convert/pandoc.js
+++ convert/pandoc.js
@@ -4,6 +4,7 @@ var source = './readme.md';
 
 var toEpub = ['-o', 'text/rachreiten.epub', 'readme.md', '--metadata-file=meta.yml',  '--toc', '--toc-depth=2', '--epub-cover-image=./cover/1.jpg', '--css=src/styles/text/pdf-styles.css']
 var toFb2 = ['-f', 'markdown', '-t', 'fb2', '-o', 'text/rachreiten.fb2'];
+var toHtml = ['-f', 'markdown', '-t', 'html', '-o', 'text/rachreiten.html'];
 
 var errorHandler = function (err, result) {
 
@@ -20,6 +21,7 @@ module.exports = {
   execute() {
     nodePandoc(source, toEpub, errorHandler);
     nodePandoc(source, toFb2, errorHandler);
+    nodePandoc(source, toHtml, errorHandler);
   }
 };
 
