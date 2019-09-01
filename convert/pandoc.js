b821f463bb9a184980438fcd70834921c45d0a8c
iwanttobealight <iwanttobealight@yandex.ru>
2019-09-01 19:22:35 +0300

    move epub convertation to separate file

--- convert/pandoc.js
+++ convert/pandoc.js
@@ -2,7 +2,7 @@ var nodePandoc = require('node-pandoc');
 
 var source = './readme.md';
 
-var toEpub = ['-o', 'text/rachreiten.epub', 'readme.md', '--metadata-file=meta.yml',  '--toc', '--toc-depth=2', '--epub-cover-image=./cover/1.jpg', '--css=src/styles/text/pdf-styles.css']
+// var toEpub = ['-o', 'text/rachreiten.epub', 'readme.md', '--metadata-file=meta.yml',  '--toc', '--toc-depth=2', '--epub-cover-image=./cover/1.jpg', '--css=src/styles/text/pdf-styles.css']
 var toFb2 = ['-f', 'markdown', '-t', 'fb2', '-o', 'text/rachreiten.fb2'];
 var toHtml = ['-f', 'markdown', '-t', 'html', '-o', 'text/rachreiten.html'];
 
@@ -19,7 +19,6 @@ var errorHandler = function (err, result) {
 module.exports = {
   name: 'pandoc',
   execute() {
-    nodePandoc(source, toEpub, errorHandler);
     nodePandoc(source, toFb2, errorHandler);
     nodePandoc(source, toHtml, errorHandler);
   }
