8891299f0c438e3b3eefa2910b53edfc7e980a17
iwanttobealight <iwanttobealight@yandex.ru>
2019-09-11 07:33:31 +0300

    fix path

--- convert/pandoc.js
+++ convert/pandoc.js
@@ -2,7 +2,7 @@ var nodePandoc = require('node-pandoc');
 
 var source = './readme.md';
 
-var toEpub = ['-o', 'text/rachreiten.epub', 'readme.md', '--metadata-file=meta.yml', `--epub-cover-image=./../src/cover/${Math.random() > 0.5 ? 1 : 2}.jpg`]
+var toEpub = ['-o', 'text/rachreiten.epub', 'readme.md', '--metadata-file=meta.yml', `--epub-cover-image=src/cover/${Math.random() > 0.5 ? 1 : 2}.jpg`]
 var toFb2 = ['-f', 'markdown', '-t', 'fb2', '-o', './text/rachreiten.fb2'];
 var toHtml = ['-f', 'markdown', '-t', 'html', '-o', './text/rachreiten.html'];
 
