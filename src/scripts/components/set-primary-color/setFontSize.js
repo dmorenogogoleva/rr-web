35dd75d56c717bfb7e1497c79ffcd03ce05b2bba
iwanttobealight <iwanttobealight@yandex.ru>
2019-09-15 11:20:22 +0300

    fix font size

--- src/scripts/components/set-primary-color/setFontSize.js
+++ src/scripts/components/set-primary-color/setFontSize.js
@@ -12,9 +12,9 @@ export const setFontSize = () => {
     const percent = windowWidth / 50
 
     const elementWidth = Math.ceil(width / percent)
-    const fontSize = elementWidth < 18 ? `18px` : `${elementWidth}px`
-    root.style.setProperty('--text-size', fontSize)
-    root.style.setProperty('--label-font-size', fontSize * 0.2)
+    const fontSize = elementWidth < 18 ? 18 : elementWidth
+    root.style.setProperty('--text-size', `${fontSize}px`)
+    root.style.setProperty('--label-font-size', `${fontSize * 1.2}px`)
   })
 
   return observer.observe(text)
