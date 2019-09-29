bc957e72a308a0a74035b2ab48296708a2294748
iwanttobealight <iwanttobealight@yandex.ru>
2019-09-29 18:13:46 +0300

    update convert package

--- convert.js
+++ convert.js
@@ -1,5 +1,5 @@
 const pandoc = require('./convert/pandoc');
 const convertToPdf = require('./convert/convertToPdf');
 
-convertToPdf.execute()
-pandoc.execute()
+convertToPdf.execute();
+pandoc.execute();
--- convert/pandoc.js
+++ convert/pandoc.js
@@ -1,44 +1,39 @@
-var nodePandoc = require('node-pandoc');
-
-var source = './readme.md';
+const pandoc = require('simple-pandoc');
+const fs = require('fs');
 
 var getCoverNum = function() {
   const num = Math.random();
-  
+
   if (num < 0.33) {
-    return 1
+    return 1;
   }
 
   if (num < 0.66) {
-    return 2
+    return 2;
   }
 
   if (num < 1) {
-    return 3
+    return 3;
   }
-}
-
-var toEpub = ['-o', 'text/rachreiten.epub', 'readme.md', '--metadata-file=meta.yml', `--epub-cover-image=src/cover/${getCoverNum()}.jpg`]
-var toFb2 = ['-f', 'markdown', 'readme.md', '-t', 'fb2', '-o', './text/rachreiten.fb2'];
-var toHtml = ['-f', 'markdown', '-t', 'html', '-o', './text/rachreiten.html'];
-
-var errorHandler = function (err, result) {
-
-  if (err) {
-    console.error('Oh Nos: ', err);
-  }
-
-  console.log(result);
-  return result;
 };
+
+const input = fs.createReadStream('readme.md');
+
+const mdToFb2 = pandoc('markdown', 'fb2');
+const mdToEpub = pandoc(
+  'markdown',
+  'epub',
+  '--metadata-file=meta.yml',
+  `--epub-cover-image=src/cover/${getCoverNum()}.jpg`,
+);
+
+const outputfb2 = fs.createWriteStream('./text/rachreiten.fb2');
+const outputEpub = fs.createWriteStream('./text/rachreiten.epub');
 
 module.exports = {
   name: 'pandoc',
   execute() {
-    nodePandoc(source, toHtml, errorHandler);
-    nodePandoc(source, toFb2, errorHandler);
-    nodePandoc(source, toEpub, errorHandler);
-  }
+    mdToFb2.stream(input).pipe(outputfb2);
+    mdToEpub.stream(input).pipe(outputEpub);
+  },
 };
-
-
